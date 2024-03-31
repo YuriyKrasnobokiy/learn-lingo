48ff19a88fc0e0543449ff26d4b12a38da199e77
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-03-31 21:37:38 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/learn-lingo@f7036f11be1139fe83e5d063d06c4552d3354be8 🚀

--- assets/Home-Brh27qnK.js
+++ assets/Home-Brh27qnK.js
@@ -1,3 +1,3 @@
-import{u as t,j as e}from"./index-Kca8ru8V.js";const s=t.p`
+import{u as t,j as e}from"./index-CG6Lb5bL.js";const s=t.p`
   color: ${o=>o.theme.colors.textColor};
 `,a=()=>e.jsx("div",{className:"container",children:e.jsx(s,{children:"It`s Home Page"})});export{a as default};
---
+++ assets/Teachers-GYnknkUG.js
@@ -0,0 +1,3 @@
+import{u as r,j as e}from"./index-CG6Lb5bL.js";const t=r.p`
+  color: ${s=>s.theme.colors.textColor};
+`,a=()=>e.jsx("div",{className:"container",children:e.jsx(t,{children:"This is a Teachers page"})});export{a as default};
