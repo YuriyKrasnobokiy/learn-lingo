4421a03515d58fcd924589d82f6dde9c9143f40e
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-01 23:11:37 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/learn-lingo@fd73a89ba7b3dd67328eb36e8291bfaf28aad793 🚀

---
+++ assets/Home-DDmsCnCB.js
@@ -0,0 +1,74 @@
+import{u as o,j as e}from"./index-lC_UTypo.js";const s=o.div`
+  display: grid;
+  grid-template-columns: 1fr 568px;
+  gap: 24px;
+`,c=o.div`
+  padding: 98px 108px 98px 64px;
+  border-radius: 30px;
+  background-color: ${t=>t.theme.colors.HomeTextBlkBGC};
+`,l=o.h1`
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 1.16667;
+  letter-spacing: -0.02em;
+  margin-bottom: 32px;
+`,a=o.span`
+  border-radius: 14px;
+  background-color: ${t=>t.theme.colors.thirdAccentColor};
+`,d=o.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.375;
+  letter-spacing: -0.02em;
+  margin-bottom: 64px;
+  width: 471px;
+`,x=o.button`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 1.55;
+  border-radius: 12px;
+  padding: 16px 88px;
+  margin: 0;
+  color: ${t=>t.theme.colors.textColor};
+  background-color: ${t=>t.theme.colors.accentColor};
+  transition: background-color 0.3s linear;
+
+  &:hover {
+    background-color: ${t=>t.theme.colors.secondAccentColor};
+  }
+`,h=o.div`
+  /* width: 568px;
+  height: 530px;
+  border-radius: 30px;
+  background-color: ${t=>t.theme.colors.HomeImgBlkBGC}; */
+`,g=o.div`
+  padding: 40px 0;
+  border: 1.5px dashed ${t=>t.theme.colors.accentColor};
+  border-radius: 30px;
+
+  margin-top: 24px;
+`,p=o.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`,r=o.li`
+  display: flex;
+  align-items: center;
+
+  &:not(:last-child) {
+    margin-right: 100px;
+  }
+`,i=o.p`
+  font-weight: 500;
+  font-size: 28px;
+  line-height: 1.14;
+  letter-spacing: -0.02em;
+  margin-right: 16px;
+`,n=o.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.28;
+  letter-spacing: -0.02em;
+  max-width: 106px;
+  color: ${t=>t.theme.colors.StatsDescrColor};
+`,m="/learn-lingo/assets/smile-C_OMyEqn.svg",j=()=>e.jsxs("div",{className:"container",children:[e.jsxs(s,{children:[e.jsxs(c,{children:[e.jsxs(l,{children:["Unlock your potential with the best"," ",e.jsx(a,{children:e.jsx("i",{children:"language"})})," ","tutors"]}),e.jsx(d,{children:"Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors."}),e.jsx(x,{type:"button",children:"Get started"})]}),e.jsx(h,{children:e.jsx("img",{src:m,alt:"smilling girl"})})]}),e.jsx(g,{children:e.jsxs(p,{children:[e.jsxs(r,{children:[e.jsx(i,{children:"32,000 + "}),e.jsx(n,{children:"Experienced tutors"})]}),e.jsxs(r,{children:[e.jsx(i,{children:"300,000 + "}),e.jsx(n,{children:"5-star tutor reviews"})]}),e.jsxs(r,{children:[e.jsx(i,{children:"120 + "}),e.jsx(n,{children:"Subjects taught"})]}),e.jsxs(r,{children:[e.jsx(i,{children:"200 + "}),e.jsx(n,{children:"Tutor nationalities"})]})]})})]});export{j as default};
