59522ab075a7dc23bb11406f2080082f78d0ab80
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2024-04-05 00:49:21 +0300

    added logaut and user menu

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   AuthBtnsWrap,
   LoginBtnIcon,
@@ -9,10 +9,32 @@ import { RegisterForm } from "../RegisterForm/RegisterForm";
 import { Modal } from "../Modal/Modal";
 import LoginIcon from "../../assets/icons/login.svg";
 import { LoginForm } from "../LoginForm/LoginForm";
+import { auth } from "../firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 export const Auth = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => {
+      listen();
+    };
+  }, []);
+
+  const userSignOut = () => {
+    signOut(auth)
+      .then(() => console.log("Successfully signed out"))
+      .catch((error) => console.log(error));
+  };
 
   const openModal = () => {
     setIsOpenModal((prevState) => !prevState);
@@ -28,25 +50,41 @@ export const Auth = () => {
     setIsOpenModal(true);
   };
 
-  return (
-    <AuthBtnsWrap>
-      <LoginButtonStyled type="button" onClick={openLoginForm}>
-        <LoginBtnIcon src={LoginIcon} alt="login arrow" />
-        Log In
-      </LoginButtonStyled>
-      <RegisterButtonStyled type="button" onClick={openRegisterForm}>
-        Registration
-      </RegisterButtonStyled>
+  //   <div>
+  //   <p>{`Hello ${authUser.email}`}</p>
+  //   <button type="button" onClick={userSignOut}>Sign Out</button>
+  // </div>
 
-      {isOpenModal && (
-        <Modal openModal={openModal}>
-          {isLoginFormOpen ? (
-            <LoginForm openModal={openModal} />
-          ) : (
-            <RegisterForm openModal={openModal} />
+  return (
+    <>
+      {authUser ? (
+        <div>
+          <p>{`Hello ${authUser.email}`}</p>
+          <button type="button" onClick={userSignOut}>
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <AuthBtnsWrap>
+          <LoginButtonStyled type="button" onClick={openLoginForm}>
+            <LoginBtnIcon src={LoginIcon} alt="login arrow" />
+            Log In
+          </LoginButtonStyled>
+          <RegisterButtonStyled type="button" onClick={openRegisterForm}>
+            Registration
+          </RegisterButtonStyled>
+
+          {isOpenModal && (
+            <Modal openModal={openModal}>
+              {isLoginFormOpen ? (
+                <LoginForm openModal={openModal} />
+              ) : (
+                <RegisterForm openModal={openModal} />
+              )}
+            </Modal>
           )}
-        </Modal>
+        </AuthBtnsWrap>
       )}
-    </AuthBtnsWrap>
+    </>
   );
 };
