0da1eaf197665861949ff027beee9f6121fd69e9
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-04 22:13:10 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/learn-lingo@3613a8dcc64e1d22c3fc3d9d9a22d4b2d8047295 🚀

--- assets/Teachers-CsMqlXAp.js
+++ assets/Teachers-CsMqlXAp.js
@@ -1,3 +1,3 @@
-import{u as r,j as e}from"./index-CaoJ5fF2.js";const t=r.p`
+import{u as r,j as e}from"./index-9yisK1kn.js";const t=r.p`
   color: ${s=>s.theme.colors.textColor};
 `,a=()=>e.jsx("div",{className:"container",children:e.jsx(t,{children:"This is a Teachers page"})});export{a as default};
