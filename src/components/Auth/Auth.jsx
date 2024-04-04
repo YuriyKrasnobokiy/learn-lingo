f9e51351a88ac9f699061367e6dfc3e3dc1da259
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2024-04-05 01:08:58 +0300

    changed user menu and update gitignore

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   AuthBtnsWrap,
   LoginBtnIcon,
@@ -9,32 +9,10 @@ import { RegisterForm } from "../RegisterForm/RegisterForm";
 import { Modal } from "../Modal/Modal";
 import LoginIcon from "../../assets/icons/login.svg";
 import { LoginForm } from "../LoginForm/LoginForm";
-import { auth } from "../firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
 
 export const Auth = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
-  const [authUser, setAuthUser] = useState(null);
-
-  useEffect(() => {
-    const listen = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setAuthUser(user);
-      } else {
-        setAuthUser(null);
-      }
-    });
-    return () => {
-      listen();
-    };
-  }, []);
-
-  const userSignOut = () => {
-    signOut(auth)
-      .then(() => console.log("Successfully signed out"))
-      .catch((error) => console.log(error));
-  };
 
   const openModal = () => {
     setIsOpenModal((prevState) => !prevState);
@@ -50,41 +28,25 @@ export const Auth = () => {
     setIsOpenModal(true);
   };
 
-  //   <div>
-  //   <p>{`Hello ${authUser.email}`}</p>
-  //   <button type="button" onClick={userSignOut}>Sign Out</button>
-  // </div>
-
   return (
-    <>
-      {authUser ? (
-        <div>
-          <p>{`Hello ${authUser.email}`}</p>
-          <button type="button" onClick={userSignOut}>
-            Sign Out
-          </button>
-        </div>
-      ) : (
-        <AuthBtnsWrap>
-          <LoginButtonStyled type="button" onClick={openLoginForm}>
-            <LoginBtnIcon src={LoginIcon} alt="login arrow" />
-            Log In
-          </LoginButtonStyled>
-          <RegisterButtonStyled type="button" onClick={openRegisterForm}>
-            Registration
-          </RegisterButtonStyled>
+    <AuthBtnsWrap>
+      <LoginButtonStyled type="button" onClick={openLoginForm}>
+        <LoginBtnIcon src={LoginIcon} alt="login arrow" />
+        Log In
+      </LoginButtonStyled>
+      <RegisterButtonStyled type="button" onClick={openRegisterForm}>
+        Registration
+      </RegisterButtonStyled>
 
-          {isOpenModal && (
-            <Modal openModal={openModal}>
-              {isLoginFormOpen ? (
-                <LoginForm openModal={openModal} />
-              ) : (
-                <RegisterForm openModal={openModal} />
-              )}
-            </Modal>
+      {isOpenModal && (
+        <Modal openModal={openModal}>
+          {isLoginFormOpen ? (
+            <LoginForm openModal={openModal} />
+          ) : (
+            <RegisterForm openModal={openModal} />
           )}
-        </AuthBtnsWrap>
+        </Modal>
       )}
-    </>
+    </AuthBtnsWrap>
   );
 };
---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,42 @@
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useEffect } from "react";
+import { auth } from "../firebase";
+import { Auth } from "../Auth/Auth";
+
+export const UserMenu = () => {
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+    return () => {
+      listen();
+    };
+  }, []);
+
+  const userSignOut = () => {
+    signOut(auth)
+      .then(() => console.log("Successfully signed out"))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <>
+      {authUser ? (
+        <div>
+          <p>{`Hello ${authUser.email}`}</p>
+          <button type="button" onClick={userSignOut}>
+            Sign Out
+          </button>
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
+  );
+};
