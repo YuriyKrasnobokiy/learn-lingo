7d04f99a4abb56c9fb37d41f0e4e961da14bbdbb
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-05 10:26:44 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/learn-lingo@5d9647a3013869a8048619ea2cda81873c70d3cc 🚀

--- assets/Teachers-mMHFSg_7.js
+++ assets/Teachers-mMHFSg_7.js
@@ -1,3 +1,3 @@
-import{u as r,j as e}from"./index-CWfDb-Xh.js";const t=r.p`
+import{u as r,j as e}from"./index-SnvTDMGb.js";const t=r.p`
   color: ${s=>s.theme.colors.textColor};
 `,a=()=>e.jsx("div",{className:"container",children:e.jsx(t,{children:"This is a Teachers page"})});export{a as default};
