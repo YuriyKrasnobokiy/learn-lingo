5d9647a3013869a8048619ea2cda81873c70d3cc
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2024-04-05 13:25:34 +0300

    changed firebase config

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,24 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-const API_KEY = import.meta.env.VITE_API_KEY;
-const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
-const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
-const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
-const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
-const APP_ID = import.meta.env.VITE_APP_ID;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
+  apiKey: "AIzaSyCy12rInr6ozCwte4LMiTw8pyvIAouPpRs",
+  authDomain: "learn-lingo-4060d.firebaseapp.com",
+  projectId: "learn-lingo-4060d",
+  storageBucket: "learn-lingo-4060d.appspot.com",
+  messagingSenderId: "681665427932",
+  appId: "1:681665427932:web:23344a00a95aa3c8303719",
 };
 
 // Initialize Firebase
