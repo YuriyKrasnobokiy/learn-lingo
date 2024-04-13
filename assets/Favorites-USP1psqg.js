36c52a34735ed934f3b0da3fb2dfcbf7c7d943a4
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-13 12:56:11 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/learn-lingo@5177e18a0f96852ade1b3900e82fa56c358d090d 🚀

---
+++ assets/Favorites-USP1psqg.js
@@ -0,0 +1,15 @@
+import{u as s,b as o,c as n,j as e}from"./index-DBQljGby.js";import{s as c,T as m,a as l}from"./TeacherCard-cTNORej2.js";const p="/learn-lingo/assets/empty-favorite-S6TsBk7o.svg",x=s.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1312px;
+  margin: 0 auto;
+`,h=s.img`
+  border-radius: 50%;
+  width: 520px;
+`,d=s.h2`
+  font-size: 40px;
+  line-height: 1.2;
+  letter-spacing: -0.02em;
+  margin-top: 20px;
+`,f=()=>{const i=o(c),a=n.currentUser.uid,r=i.filter(t=>t.owner===a);return e.jsx(e.Fragment,{children:r.length>0?e.jsx(m,{children:e.jsx("ul",{children:r.map(t=>e.jsx(l,{teacher:t,owner:t.owner},t.id))})}):e.jsx(e.Fragment,{children:e.jsxs(x,{children:[e.jsx(h,{src:p,alt:"empty"}),e.jsx(d,{children:"Oops! It looks like this section is empty at the moment..."})]})})})};export{f as default};
