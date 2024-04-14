047072400d4f1ce921ccb00e143096db2ca11c27
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-14 10:37:02 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/learn-lingo@7b20f3fac4b85e82f6dfe752e4fa26496023f581 🚀

--- assets/Favorites-WMkibnL0.js
+++ assets/Favorites-WMkibnL0.js
@@ -1,4 +1,4 @@
-import{u as s,b as o,c as n,j as e}from"./index-DIqlvuMU.js";import{s as c,T as m,a as l}from"./TeacherCard-fpKtg8CF.js";const p="/learn-lingo/assets/empty-favorite-S6TsBk7o.svg",x=s.div`
+import{u as s,b as o,c as n,j as e}from"./index-DImWaUvI.js";import{s as c,T as m,a as l}from"./TeacherCard-B7w8q_SG.js";const p="/learn-lingo/assets/empty-favorite-S6TsBk7o.svg",x=s.div`
   display: flex;
   flex-direction: column;
   align-items: center;
